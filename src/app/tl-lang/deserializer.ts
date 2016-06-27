fb3afe980cfc9deec5f1f31355795d39a54a55c7
Herrington Darkholme <nonamesheep1@gmail.com>
2016-06-28 01:13:47 +0800

    update deserializer

--- src/app/tl-lang/deserializer.ts
+++ src/app/tl-lang/deserializer.ts
@@ -1,6 +1,19 @@
 import {Injectable} from '@angular/core'
 import {bytesToHex} from '../crypto'
 
+const SUCCESS = true
+export const BASE_TYPES = {
+  '#'      : 'getInt',
+  'int'    : 'getInt',
+  'long'   : 'getLong',
+  'int128' : 'getInt128',
+  'int256' : 'getInt256',
+  'string' : 'getString',
+  'bytes'  : 'getBytes',
+  'double' : 'getDouble',
+  'Bool'   : 'getBool',
+}
+
 @Injectable()
 export class Deserializer {
 
@@ -93,4 +106,78 @@ export class Deserializer {
   getInt256() {
     return this._getBigIntN(32)
   }
+
+  getBool() {
+    let bool = this.getInt()
+    if (bool === 0x997275b5) {
+      return true
+    }
+    if (bool === 0xbc799737) {
+      return false
+    }
+    throw new TypeError('invalid bool value')
+  }
+
+  private _tryGetBase(type: string): [boolean, any] {
+    if (type in BASE_TYPES) {
+      let ret = this[BASE_TYPES[type]]()
+      return [true, ret]
+    }
+    if (type === 'true') {
+      return [true, true]
+    }
+    return [false, undefined]
+  }
+
+  private _tryGetVector(type: string, schema): [boolean, any] {
+    let vectorName = type.substr(0, 6)
+    if (vectorName.toLowerCase() !== 'vector') {
+      return [false, undefined]
+    }
+
+    if (vectorName === 'Vector') {
+      let constructorCmp = uintToInt(this.getInt())
+      // gzip
+      if (constructorCmp === 0x3072cfa1) {
+        throw new TypeError('unsupported for now')
+        // let compressed = this.getBytes()
+        // let uncompressed = gzipUncompress(compressed)
+        // let newDeserializer = new Deserializer(uncompressed)
+        // return newDeserializer.getObject(type, field);
+      }
+      if (constructorCmp !== 0x1cb5c415) {
+        throw new TypeError('invalid vector constructor')
+      }
+    }
+    let length = this.getInt()
+    let result = []
+    if (length <= 0) {
+      return [true, result]
+    }
+    let itemType = type.substr(7, type.length - 8)
+    for (let i = 0; i < length; i++) {
+      result.push(this.getObject(type, schema))
+    }
+    return [true, result]
+  }
+
+  getObject(type: string, schema) {
+    let [success, ret] = this._tryGetBase(type)
+    if (success) {
+      return ret
+    }
+    ;[success, ret] = this._tryGetVector(type, schema)
+    if (success) {
+      return ret
+    }
+    // todo
+    throw new Error('TODO')
+  }
+}
+
+function uintToInt(val) {
+  if (val > 2147483647) {
+    val = val - 4294967296
+  }
+  return val
 }
